4f3bbcd751bd902b4c375ad42b92af62f92e866f
IKA TI21f <114545046+ikasalbella@users.noreply.github.com>
2023-02-04 17:41:59 +0700

    Add files via upload

---
+++ Exception3.java
@@ -0,0 +1,21 @@
+
+/**
+ * Nama: Ika
+ * NIM: 20210040096
+ * Kelas: TI21F
+ * @author Asus
+ *  PBO Sesi 11
+ */
+public class Exception3 {
+   public static void main(String[] args) {
+ int bil=10;
+ try
+ {
+ System.out.println(bil/0);
+ }
+ catch(Exception e)
+ {
+ System.out.println("Ini menghandle error yang terjadi");
+ }
+ }
+} 
