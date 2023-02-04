96a830d2f82915c3e6dec8242499b97ca1dbfc17
IKA TI21f <114545046+ikasalbella@users.noreply.github.com>
2023-02-04 17:55:37 +0700

    Add files via upload

---
+++ Satu.java
@@ -0,0 +1,14 @@
+
+/**
+ * Nama: Ika
+ * NIM: 20210040096
+ * Kelas: TI21F
+ * @author Asus
+ *  PBO Sesi 11
+ */
+public class Satu {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        a[5]=100;
+    }
+}
